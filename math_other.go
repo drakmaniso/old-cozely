32b983b91aa7d7344b01eba8daa5ca248f826b54
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-14 19:20:43 +0200

    Default Floor implementation.

--- math_other.go
+++ math_other.go
@@ -14,4 +14,10 @@ func Sqrt(x float32) float32 {
 }
 
 //------------------------------------------------------------------------------
+
+func Floor(x float32) float32 {
+	return float32(math.Floor(float64(x)))
+}
+
+//------------------------------------------------------------------------------
 // Copyright (c) 2013 - Laurent Moussault <moussault.laurent@gmail.com>
