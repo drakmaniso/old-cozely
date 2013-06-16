1ae4035fedd913666de6be3e6faeca8295c86869
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-16 18:50:21 +0200

    Removed fallback implementations.
    Should not compile on unsupported platforms.

--- math.go
+++ math.go
@@ -1,8 +1,6 @@
 // Copyright (c) 2013 Laurent Moussault. All rights reserved.
 // Licensed under a simplified BSD license (see LICENSE file).
 
-// +build 386 amd64
-
 package glm
 
 //------------------------------------------------------------------------------
