89f2d726c05fbb7aa4b68b3d163015575d5c6be7
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-17 16:14:19 +0200

    Moved Round into its own files.

--- math/round_amd64.s
+++ math/round_amd64.s
@@ -1,7 +1,5 @@
-// Based on code from the Go standard library.
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the ORIGINAL_LICENSE file.
+// Copyright (c) 2013 Laurent Moussault. All rights reserved.
+// Licensed under a simplified BSD license (see LICENSE file).
 
 //------------------------------------------------------------------------------
 
@@ -9,7 +7,7 @@
 // func asmRound(s float32) float32
 TEXT ·asmRound(SB),7,$0
 	CVTSS2SL  x+0(FP), BX
-	MOVL       BX, ret+4(FP)
+	MOVL       BX, ret+8(FP)
 	RET
 
 //------------------------------------------------------------------------------
