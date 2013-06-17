1cbb0b3d90d69c7f065c9a610d663d30f6aa07ca
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-17 15:58:25 +0200

    Changed copyright notice.

--- math/math_amd64.s
+++ math/math_amd64.s
@@ -1,7 +1,7 @@
-// Copyright (c) 2013 Laurent Moussault. All rights reserved.
-// Licensed under a simplified BSD license (see LICENSE file).
-
-// Based on code from the Go standard library: see ORIGINAL_LICENSE file.
+// Based on code from the Go standard library.
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the ORIGINAL_LICENSE file.
 
 //------------------------------------------------------------------------------
 
