3dc7cb46e2793f601862af09ab4bd6b86e63e1ac
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-17 16:02:09 +0200

    Moved Sqrt into its own file.

--- math/math.go
+++ math/math.go
@@ -5,13 +5,6 @@ package math
 
 //------------------------------------------------------------------------------
 
-//go:noescape
-
-// Sqrt returns the square root of x.
-func Sqrt(x float32) float32
-
-//------------------------------------------------------------------------------
-
 // Mix returns the linear interpolation between x and y using a to weight between them.
 // The value is computed as follows: x*(1-a) + y*a
 func Mix(x, y float32, a float32) float32 {
---
+++ math/sqrt.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2013 Laurent Moussault. All rights reserved.
+// Licensed under a simplified BSD license (see LICENSE file).
+
+package math
+
+//------------------------------------------------------------------------------
+
+//go:noescape
+
+// Sqrt returns the square root of x.
+func Sqrt(x float32) float32
+
+//------------------------------------------------------------------------------
