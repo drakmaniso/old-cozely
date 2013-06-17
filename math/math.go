c166a4bccdbeb8a1121418540d60770371ebd4e8
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-17 16:04:15 +0200

    Moved mix into its own file.

--- math/math.go
+++ math/math.go
@@ -5,14 +5,6 @@ package math
 
 //------------------------------------------------------------------------------
 
-// Mix returns the linear interpolation between x and y using a to weight between them.
-// The value is computed as follows: x*(1-a) + y*a
-func Mix(x, y float32, a float32) float32 {
-	return x*(1-a) + y*a
-}
-
-//------------------------------------------------------------------------------
-
 //go:noescape
 
 // Floor returns the nearest integer less than or equal to x.
