c828689a4b5e30c0a6a8163c9ad196b0051098c8
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-19 21:41:36 +0200

    Fixed copy/paste bug.

--- math/nearlyequal.go
+++ math/nearlyequal.go
@@ -11,22 +11,30 @@ package math
 // Handles special cases: zero, infinites, denormals.
 func NearlyEqual(a, b float32, epsilon float32) bool {
 	// Source: http://floating-point-gui.de/errors/comparison/
-	
-	if a < 0 { a = -a }
-	if b < 0 { b = -b }
+
+	absA := a
+	if absA < 0 {
+		absA = -absA
+	}
+	absB := b
+	if absB < 0 {
+		absB = -absB
+	}
 	diff := a - b
-	if diff < 0 { diff = - diff }
-	
-	if a == b { 
+	if diff < 0 {
+		diff = -diff
+	}
+
+	if a == b {
 		// Shortcut, handles infinities.
 		return true
 	} else if a == 0 || b == 0 || diff < SmallestNormalFloat32 {
 		// a or b is zero or both are extremely close to it.
 		// Relative error is less meaningful here.
 		return diff < (epsilon * SmallestNormalFloat32)
-	} else { 
+	} else {
 		// Use relative error.
-		return diff / (absA + absB) < epsilon;
+		return diff/(absA+absB) < epsilon
 	}
 }
 
