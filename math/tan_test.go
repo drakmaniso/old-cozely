d183f290af7450897db8b8debbd0d3966d230719
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-19 21:43:46 +0200

    Use NearlyEqual in unit tests for Tan.

--- math/tan_test.go
+++ math/tan_test.go
@@ -10,56 +10,50 @@ import (
 
 //------------------------------------------------------------------------------
 
-func near(a, b float32) bool {
-	const tolerance = float32(1e-5)
-	return a < b-tolerance || b+tolerance < a
-}
-
-//------------------------------------------------------------------------------
-
 func TestTan(t *testing.T) {
+	const epsilon = float32(1e-7)
 	a := Tan(0)
 	if a != 0 {
 		t.Errorf("Wrong result for Tan(0): %v\n", a)
 	}
 	b := Tan(0.2)
-	if near(b, 0.202710035509) {
+	if !NearlyEqual(b, 0.202710035509, epsilon) {
 		t.Errorf("Wrong result for Tan(0.2): %v\n", b)
 	}
 	c := Tan(0.5)
-	if near(c, 0.546302489844) {
+	if !NearlyEqual(c, 0.546302489844, epsilon) {
 		t.Errorf("Wrong result for Tan(0.5): %v\n", c)
 	}
 	d := Tan(Pi / 4)
-	if near(d, 1) {
+	if !NearlyEqual(d, 1, epsilon) {
 		t.Errorf("Wrong result for Tan(Pi/4): %v\n", d)
 	}
 	e := Tan(Pi / 3)
-	if near(e, 1.73205080757) {
+	if !NearlyEqual(e, 1.73205080757, epsilon) {
 		t.Errorf("Wrong result for Tan(Pi/3): %v\n", e)
 	}
 	f := Tan(Pi/2 - 0.1)
-	if near(f, 9.96664442326) {
+	if !NearlyEqual(f, 9.96664442326, epsilon) {
 		t.Errorf("Wrong result for Tan(Pi/2 - 0.1): %v\n", f)
 	}
 	g := Tan(-0.2)
-	if near(g, -0.202710035509) {
+	if !NearlyEqual(g, -0.202710035509, epsilon) {
 		t.Errorf("Wrong result for Tan(-0.2): %v\n", g)
 	}
 	h := Tan(-0.5)
-	if near(h, -0.546302489844) {
+	if !NearlyEqual(h, -0.546302489844, epsilon) {
 		t.Errorf("Wrong result for Tan(-0.5): %v\n", h)
 	}
 	i := Tan(-Pi / 4)
-	if near(i, -1) {
+	if !NearlyEqual(i, -1, epsilon) {
 		t.Errorf("Wrong result for Tan(-Pi/4): %v\n", i)
 	}
 	j := Tan(-Pi / 3)
-	if near(j, -1.73205080757) {
+	if !NearlyEqual(j, -1.73205080757, epsilon) {
 		t.Errorf("Wrong result for Tan(-Pi/3): %v\n", j)
 	}
 	k := Tan(-Pi/2 + 0.1)
-	if near(k, -9.96664442326) {
+	if !NearlyEqual(k, -9.96664442326, epsilon) {
 		t.Errorf("Wrong result for Tan(-Pi/2 + 0.1): %v\n", k)
 	}
 }
