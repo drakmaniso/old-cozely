4bdb5f95661e160d4d4a2bc8120989db3364a0a2
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-20 08:54:05 +0200

    Removed half-written doc.

--- doc.go
+++ doc.go
@@ -1,13 +1,3 @@
-// Package glm provides mathematical types and operations for use with OpenGL.
-//
-//- Type names mirroring GLSL types: Vec2, Vec3, Vec4, Mat3, Mat4, IVec3...
-//
-//- All types are pure values: there's no heap allocation, and no hidden data.
-//
-//- All types have the same memory layout than their corresponding GLSL types.
-//
-//- Most methods are inlined by the compiler.
-//
-//- Efficient single-precision math (use assembly).
+// Package GLaM provides mathematical types and operations for use with OpenGL.
 //
 package glam
