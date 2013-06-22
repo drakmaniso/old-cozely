fe995a2c0c9eb72800df533b19c90b73c099d69e
Laurent Moussault <moussault.laurent@gmail.com>
2013-06-22 23:03:47 +0200

    Added IsRoughlyEqual.

---
+++ math/isroughlyequal.go
@@ -0,0 +1,19 @@
+// Copyright (c) 2013 Laurent Moussault. All rights reserved.
+// Licensed under a simplified BSD license (see LICENSE file).
+
+package math
+
+//------------------------------------------------------------------------------
+
+// `IsRoughlyEqual` Returns true if the absolute error between `a` and `b` is less than `epsilon`.
+func IsRoughlyEqual(a, b float32, epsilon float32) bool {
+	if a == b {
+		// Shortcut, handles infinities
+		return true
+	} else {
+		// Use absolute error
+		return Abs(a - b) < epsilon
+	}
+}
+
+//------------------------------------------------------------------------------
