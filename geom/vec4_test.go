5d29bc8acac573905bea12d0321c9ea9f290b404
Laurent Moussault <moussault.laurent@gmail.com>
2016-03-06 13:04:29 +0100

    Separate tests and benchmarks

--- geom/vec4_test.go
+++ geom/vec4_test.go
@@ -1,88 +1,28 @@
 // Copyright (c) 2013-2016 Laurent Moussault. All rights reserved.
 // Licensed under a simplified BSD license (see LICENSE file).
 
-package geom
+package geom_test
 
 import (
 	"fmt"
 	"testing"
 	"unsafe"
 
-	"github.com/drakmaniso/glam/math"
+	"github.com/drakmaniso/glam/geom"
 )
 
 //-----------------------------------------------------------------------------
 
-func (v *Vec4) add(a, b Vec4) {
-	v.X = a.X + b.X
-	v.Y = a.Y + b.Y
-	v.Z = a.Z + b.Z
-	v.W = a.W + b.W
-}
-
-func (v *Vec4) subtract(a, b Vec4) {
-	v.X = a.X - b.X
-	v.Y = a.Y - b.Y
-	v.Z = a.Z - b.Z
-	v.W = a.W - b.W
-}
-
-func (v *Vec4) invert() {
-	v.X = -v.X
-	v.Y = -v.Y
-	v.Z = -v.Z
-	v.W = -v.W
-}
-
-func (v *Vec4) multiply(o Vec4, s float32) {
-	v.X = o.X * s
-	v.Y = o.Y * s
-	v.Z = o.Z * s
-	v.W = o.W * s
-}
-
-func (v *Vec4) divide(o Vec4, s float32) {
-	v.X = o.X / s
-	v.Y = o.Y / s
-	v.Z = o.Z / s
-	v.W = o.W / s
-}
-
-func (v *Vec4) normalize() {
-	length := math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z + v.W*v.W)
-	v.X /= length
-	v.Y /= length
-	v.Z /= length
-	v.W /= length
-}
-
-//-----------------------------------------------------------------------------
-
-type arrayVec4 [4]float32
-
-func (v arrayVec4) plus(o arrayVec4) arrayVec4 {
-	return arrayVec4{v[0] + o[0], v[1] + o[1], v[2] + o[2], v[3] + o[3]}
-}
-
-func (v *arrayVec4) add(a, b arrayVec4) {
-	v[0] = a[0] + b[0]
-	v[1] = a[1] + b[1]
-	v[2] = a[2] + b[2]
-	v[3] = a[3] + b[3]
-}
-
-//-----------------------------------------------------------------------------
-
 func TestVec4_Creation(t *testing.T) {
-	var a Vec4
+	var a geom.Vec4
 	if a.X != 0 || a.Y != 0 || a.Z != 0 || a.W != 0 {
 		t.Errorf("Zero-initialization failed")
 	}
-	b := Vec4{1.1, 2.2, 3.3, 4.4}
+	b := geom.Vec4{1.1, 2.2, 3.3, 4.4}
 	if b.X != 1.1 || b.Y != 2.2 || b.Z != 3.3 || b.W != 4.4 {
 		t.Errorf("Literal initialization failed")
 	}
-	c := [2]Vec4{{1, 2, 3, 4}, {5, 6, 7, 8}}
+	c := [2]geom.Vec4{{1, 2, 3, 4}, {5, 6, 7, 8}}
 	if unsafe.Pointer(&c) != unsafe.Pointer(&c[0].X) {
 		t.Errorf("Padding before c[0].X")
 	}
@@ -93,19 +33,19 @@ func TestVec4_Creation(t *testing.T) {
 		t.Errorf("Padding between c[0].Y an c[0].Z")
 	}
 	if uintptr(unsafe.Pointer(&c[0].Z))+4 != uintptr(unsafe.Pointer(&c[0].W)) {
-		t.Errorf("Padding between c[0].Z an c[1].X")
+		t.Errorf("Padding between c[0].Z an c[0].W")
 	}
 	if uintptr(unsafe.Pointer(&c[0].W))+4 != uintptr(unsafe.Pointer(&c[1].X)) {
-		t.Errorf("Padding between c[0].Z an c[1].X")
+		t.Errorf("Padding between c[0].W an c[1].X")
 	}
 }
 
 func ExampleVec4() {
-	var a Vec4
-	b := Vec4{1.1, 2.2, 3.3, 4.4}
-	c := b.Plus(Vec4{5.5, 6.6, 7.7, 8.8})
+	var a geom.Vec4
+	b := geom.Vec4{1.1, 2.2, 3.3, 4.4}
+	c := b.Plus(geom.Vec4{5.5, 6.6, 7.7, 8.8})
 	d := b
-	d = d.Plus(Vec4{5.5, 6.6, 7.7, 8.8})
+	d = d.Plus(geom.Vec4{5.5, 6.6, 7.7, 8.8})
 	e := b.Slash(2.2)
 	f := e.Dehomogenized()
 	g := b
@@ -131,7 +71,7 @@ func ExampleVec4() {
 //-----------------------------------------------------------------------------
 
 func TestVec4_Dehomogenized(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
 	b := a.Dehomogenized()
 	if b.X != 0.25 || b.Y != 0.5 || b.Z != 0.75 {
 		t.Errorf("Wrong result: %#v", b)
@@ -144,8 +84,8 @@ func TestVec4_Dehomogenized(t *testing.T) {
 //-----------------------------------------------------------------------------
 
 func TestVec4_Plus(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
-	b := Vec4{5.5, 6.6, 7.7, 8.8}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
+	b := geom.Vec4{5.5, 6.6, 7.7, 8.8}
 	c := a.Plus(b)
 	if c.X != 6.6 || c.Y != 8.8 || c.Z != 11 || c.W != 13.200001 {
 		t.Errorf("Wrong result: %#v", c)
@@ -155,87 +95,11 @@ func TestVec4_Plus(t *testing.T) {
 	}
 }
 
-func BenchmarkVec4_Plus(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := Vec4{5.5, 6.6, 7.7, 8.8}
-	var o Vec4
-	for i := 0; i < b.N; i++ {
-		o = m.Plus(n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Plus_Array(b *testing.B) {
-	m := arrayVec4{1.1, 2.2, 3.3, 4.4}
-	n := arrayVec4{5.5, 6.6, 7.7, 8.8}
-	var o arrayVec4
-	for i := 0; i < b.N; i++ {
-		o = m.plus(n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Plus_Self(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := Vec4{5.5, 6.6, 7.7, 8.8}
-	for i := 0; i < b.N; i++ {
-		m = m.Plus(n)
-	}
-	_ = m
-}
-
-func BenchmarkVec4_Plus_ArraySelf(b *testing.B) {
-	m := arrayVec4{1.1, 2.2, 3.3, 4.4}
-	n := arrayVec4{5.5, 6.6, 7.7, 8.8}
-	for i := 0; i < b.N; i++ {
-		m = m.plus(n)
-	}
-	_ = m
-}
-
-func BenchmarkVec4_Plus_ByRef(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := Vec4{5.5, 6.6, 7.7, 8.8}
-	var o Vec4
-	for i := 0; i < b.N; i++ {
-		o.add(m, n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Plus_ArrayByRef(b *testing.B) {
-	m := arrayVec4{1.1, 2.2, 3.3, 4.4}
-	n := arrayVec4{5.5, 6.6, 7.7, 8.8}
-	var o arrayVec4
-	for i := 0; i < b.N; i++ {
-		o.add(m, n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Plus_ByRefSelf(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := Vec4{5.5, 6.6, 7.7, 8.8}
-	for i := 0; i < b.N; i++ {
-		m.add(m, n)
-	}
-	_ = m
-}
-
-func BenchmarkVec4_Plus_ArrayByRefSelf(b *testing.B) {
-	m := arrayVec4{1.1, 2.2, 3.3, 4.4}
-	n := arrayVec4{5.5, 6.6, 7.7, 8.8}
-	for i := 0; i < b.N; i++ {
-		m.add(m, n)
-	}
-	_ = m
-}
-
 //-----------------------------------------------------------------------------
 
 func TestVec4_Minus(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
-	b := Vec4{5.5, 6.6, 7.7, 8.8}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
+	b := geom.Vec4{5.5, 6.6, 7.7, 8.8}
 	c := a.Minus(b)
 	if c.X != -4.4 || c.Y != -4.3999996 || c.Z != -4.3999996 || c.W != -4.4 {
 		t.Errorf("Wrong result: %#v", c)
@@ -248,7 +112,7 @@ func TestVec4_Minus(t *testing.T) {
 //-----------------------------------------------------------------------------
 
 func TestVec4_Inverse(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
 	b := a.Inverse()
 	if b.X != -1.1 || b.Y != -2.2 || b.Z != -3.3 || b.W != -4.4 {
 		t.Errorf("Wrong result: %#v", b)
@@ -261,7 +125,7 @@ func TestVec4_Inverse(t *testing.T) {
 //-----------------------------------------------------------------------------
 
 func TestVec4_Times(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
 	b := a.Times(5.5)
 	if b.X != 6.05 || b.Y != 12.1 || b.Z != 18.15 || b.W != 24.2 {
 		t.Errorf("Wrong result: %#v", b)
@@ -271,48 +135,10 @@ func TestVec4_Times(t *testing.T) {
 	}
 }
 
-func BenchmarkVec4_Times(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	var o Vec4
-	for i := 0; i < b.N; i++ {
-		o = m.Times(n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Times_Self(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	for i := 0; i < b.N; i++ {
-		m = m.Times(n)
-	}
-	_ = m
-}
-
-func BenchmarkVec4_Times_ByRef(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	var o Vec4
-	for i := 0; i < b.N; i++ {
-		o.multiply(m, n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Times_ByRefSelf(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	for i := 0; i < b.N; i++ {
-		m.multiply(m, n)
-	}
-	_ = m
-}
-
 //-----------------------------------------------------------------------------
 
 func TestVec4_Slash(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
 	b := a.Slash(5.5)
 	if b.X != 0.2 || b.Y != 0.4 || b.Z != 0.59999996 || b.W != 0.8 {
 		t.Errorf("Wrong result: %#v", b)
@@ -322,49 +148,11 @@ func TestVec4_Slash(t *testing.T) {
 	}
 }
 
-func BenchmarkVec4_Slash(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	var o Vec4
-	for i := 0; i < b.N; i++ {
-		o = m.Slash(n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Slash_Self(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	for i := 0; i < b.N; i++ {
-		m = m.Slash(n)
-	}
-	_ = m
-}
-
-func BenchmarkVec4_Slash_ByRef(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	var o Vec4
-	for i := 0; i < b.N; i++ {
-		o.divide(m, n)
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Slash_ByRefSelf(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	n := float32(5.5)
-	for i := 0; i < b.N; i++ {
-		m.divide(m, n)
-	}
-	_ = m
-}
-
 //-----------------------------------------------------------------------------
 
 func TestVec4_Dot(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
-	b := Vec4{5.5, 6.6, 7.7, 8.8}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
+	b := geom.Vec4{5.5, 6.6, 7.7, 8.8}
 	c := a.Dot(b)
 	if c != 84.7 {
 		t.Errorf("Wrong result: %#v", c)
@@ -377,7 +165,7 @@ func TestVec4_Dot(t *testing.T) {
 //-----------------------------------------------------------------------------
 
 func TestVec4_Length(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
 	b := a.Length()
 	if b != 6.024948 {
 		t.Errorf("Wrong result: %#v", b)
@@ -390,36 +178,11 @@ func TestVec4_Length(t *testing.T) {
 //-----------------------------------------------------------------------------
 
 func TestVec4_Normalized(t *testing.T) {
-	a := Vec4{1.1, 2.2, 3.3, 4.4}
+	a := geom.Vec4{1.1, 2.2, 3.3, 4.4}
 	b := a.Normalized()
 	if b.X != 0.18257418 || b.Y != 0.36514837 || b.Z != 0.5477226 || b.W != 0.73029673 {
 		t.Errorf("Wrong result: %#v", b)
 	}
-}
-
-func BenchmarkVec4_Normalized(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	var o Vec4
-	for i := 0; i < b.N; i++ {
-		o = m.Normalized()
-	}
-	_ = o
-}
-
-func BenchmarkVec4_Normalized_Self(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	for i := 0; i < b.N; i++ {
-		m = m.Normalized()
-	}
-	_ = m
-}
-
-func BenchmarkVec4_Normalize_ByRef(b *testing.B) {
-	m := Vec4{1.1, 2.2, 3.3, 4.4}
-	for i := 0; i < b.N; i++ {
-		m.normalize()
-	}
-	_ = m
 }
 
 //-----------------------------------------------------------------------------
