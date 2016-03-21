217e64f9b54aa44e13f9cb08e1a354dd300be96b
Laurent Moussault <moussault.laurent@gmail.com>
2016-03-21 19:01:15 +0100

    Rewrite geom doc

--- geom/doc.go
+++ geom/doc.go
@@ -1,31 +1,47 @@
 /*
 Package geom provides vectors and matrices, and their associated operations.
 
-It follows the conventions used by GLSL, by using similar type names, and
-most importantly, the same memory layout (i.e. column-major for matrices).
+All types defined in this package use the same memory layout than the
+corresponding GLSL type. They are pure values (no hidden data).
 
-All types are pure values: there's no heap allocation, and no hidden data.
+The notation also tries to be as close to GLSL as possible: literals use the
+same component order, function names are similar, and component access for
+matrices is identical: m[2][3] means the same thing in Go than in GLSL.
 
-Matrices
+Transformation Matrices
 
-Since they are pure values, there is no constructors, only literals. Be aware
-that they are specified and stored in column-major order, just like GLSL.
-So when writing literals, remember to use the transpose of the mathematical
-notation. In other words:
+As is usual in GLSL, to transform a vector use left-multiplication by a
+matrix:
+	T := Translation4(10, 15, 2)
+	v := Vec4{1, 2, 3}
+	vTrans := T.Transform(v)
+
+When writing literals, remember to use the transpose of the mathematical
+notation. In other words the following mathematical notation:
+	⎡ a11  a12  a13 ⎤
+	⎢ a21  a22  a23 ⎥
+	⎣ a31  a32  a33 ⎦
+Translates to:
     m := Mat3{
-		{a, b, c},
-		{d, e, f},
-		{g, h, i},
+		{a11, a21, a31},
+		{a12, a22, a32},
+		{a13, a23, a33},
     }
-... corresponds to the following mathematical notation:
-	⎡ a  d  g ⎤
-	⎢ b  e  h ⎥
-	⎣ c  f  i ⎦
 
-The same inversion happens with indices: m[column][row] corresponds to the
-mathematical indices (row,column).
+Note that the same inversion happens with indices: the last component
+of the first column is written a31 in math but accessed with m[0][2] in Go
+(and GLSL).
 
-Although all methods returns their result by value, they take their receiver and
-parameters by reference, for efficiency. They are never modified.
+Finally, although all methods returns their result by value, they take their
+receiver and parameters by reference, for efficiency. They are never modified.
+
+Note: Some describes this convention as "column-major". This can be confusing,
+because it depends on the meaning assigned to the indices of 2D arrays. E.g.
+Wikipedia describes C as row-major, but in doing so assumes that arrays are
+accessed with a[row][col]. OpenGL, defined as column-major, uses (inGLSL) the
+exact same data structure and notation than C, but assumes arrays are accessed
+with a[col][row]. What really matters is the underlying memory layout, and the
+order of matrix-vector multiplication. Yet another way to describe the
+situation is to say that vectors are treated as column-vectors.
 */
 package geom
