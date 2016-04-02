125817a44990e8b63d41ea44570f2df64d6ef977
Laurent Moussault <moussault.laurent@gmail.com>
2016-04-02 19:39:23 +0200

    Add Mat.Transpose

--- geom/matrices.go
+++ geom/matrices.go
@@ -67,6 +67,35 @@ func (m *Mat3) Mat3x4() Mat3x4 {
 
 //------------------------------------------------------------------------------
 
+// Transpose of the matrix.
+func (m *Mat4) Transpose() Mat4 {
+	return Mat4{
+		{m[0][0], m[1][0], m[2][0], m[3][0]},
+		{m[0][1], m[1][1], m[2][1], m[3][1]},
+		{m[0][2], m[1][2], m[2][2], m[3][2]},
+		{m[0][3], m[1][3], m[2][3], m[3][3]},
+	}
+}
+
+// Transpose of the matrix.
+func (m *Mat3) Transpose() Mat3 {
+	return Mat3{
+		{m[0][0], m[1][0], m[2][0]},
+		{m[0][1], m[1][1], m[2][1]},
+		{m[0][2], m[1][2], m[2][2]},
+	}
+}
+
+// Transpose of the matrix.
+func (m *Mat2) Transpose() Mat2 {
+	return Mat2{
+		{m[0][0], m[1][0]},
+		{m[0][1], m[1][1]},
+	}
+}
+
+//------------------------------------------------------------------------------
+
 // Times returns the matrix product with another matrix.
 func (m *Mat4) Times(o Mat4) Mat4 {
 	return Mat4{
