cbb770fc865810931e904bc3bd9cb15c033865d7
Laurent Moussault <moussault.laurent@gmail.com>
2017-01-13 18:52:22 +0100

    Fix comments.

--- pixel/coordinates.go
+++ pixel/coordinates.go
@@ -5,48 +5,49 @@ package pixel
 
 //------------------------------------------------------------------------------
 
-// Coord is the cartesian coordinates of a pixel on the screen.
+// Coord is the coordinates of a pixel on the screen.
 type Coord struct {
 	X, Y int32
 }
 
 //------------------------------------------------------------------------------
 
-// Cartesian implements the plane.Coord interface.
+// Cartesian returns the floating point conversion of the coordinates, thus
+// implementing the plane.Vector interface.
 func (p Coord) Cartesian() (x, y float32) {
 	return float32(p.X), float32(p.Y)
 }
 
 //------------------------------------------------------------------------------
 
-// Plus returns the sum with other coordinates.
+// Plus returns the sum with another pair of coordinates.
 func (p Coord) Plus(o Coord) Coord {
 	return Coord{p.X + o.X, p.Y + o.Y}
 }
 
-// Minus returns the difference with other coordinates.
+// Minus returns the difference with another pair of coordinates.
 func (p Coord) Minus(o Coord) Coord {
 	return Coord{p.X - o.X, p.Y - o.Y}
 }
 
-// Inverse returns the product with other coordinates.
+// Inverse returns the product with another pair of coordinates.
 func (p Coord) Inverse() Coord {
 	return Coord{-p.X, -p.Y}
 }
 
-// Times returns the product with other coordinates.
+// Times returns the product with another pair of coordinates.
 func (p Coord) Times(o Coord) Coord {
 	return Coord{p.X * o.X, p.Y * o.Y}
 }
 
-// Slash returns the integer quotient of the division by other coordinates (of
-// which both X and Y must be non-zero).
+// Slash returns the integer quotient of the division by another pair of
+// coordinates (of which both X and Y must be non-zero).
 func (p Coord) Slash(o Coord) Coord {
 	return Coord{p.X / o.X, p.Y / o.Y}
 }
 
-// Mod returns the remainder (modulus) of the division by other coordinates (of
-// which both X and Y must be non-zero).
+// Mod returns the remainder (modulus) of the division by another pair of
+// coordinates (of which both X and Y must be non-zero).
 func (p Coord) Mod(o Coord) Coord {
 	return Coord{p.X % o.X, p.Y % o.Y}
 }
