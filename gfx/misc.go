fb8bf79cc3b937a0663554f1ffb5b18f589e1a33
Laurent Moussault <moussault.laurent@gmail.com>
2017-01-24 11:37:48 +0100

    Add `DepthRange`

--- gfx/misc.go
+++ gfx/misc.go
@@ -11,6 +11,10 @@ package gfx
 static void Viewport(GLint x,  GLint y,  GLsizei width,  GLsizei height) {
 	glViewport(x, y, width, height);
 }
+
+static void DepthRange(GLdouble n, GLdouble f) {
+	glDepthRange(n, f);
+}
 */
 import "C"
 
@@ -20,12 +24,20 @@ import (
 
 //------------------------------------------------------------------------------
 
-// Viewport set the size in pixels of the GL viewport.
+// Viewport sets the size in pixels of the GL viewport.
 //
 // Note that this function is automatically called each time the window is
 // resized.
 func Viewport(orig, size pixel.Coord) {
 	C.Viewport(C.GLint(orig.X), C.GLint(orig.Y), C.GLsizei(size.X), C.GLsizei(size.Y))
+}
+
+//------------------------------------------------------------------------------
+
+// DepthRange specifies the mapping of depth values from normalized device
+// coordinates to window coordinates.
+func DepthRange(near, far float64) {
+	C.DepthRange(C.GLdouble(near), C.GLdouble(far))
 }
 
 //------------------------------------------------------------------------------
