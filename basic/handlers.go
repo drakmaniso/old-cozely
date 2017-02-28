23aa8df00db6ef70e7c304516eeb43632de0094e
Laurent Moussault <moussault.laurent@gmail.com>
2017-02-28 18:49:02 +0100

    Added another for fullscreen toggle

--- basic/handlers.go
+++ basic/handlers.go
@@ -87,7 +87,7 @@ func (dh KeyHandler) KeyDown(l key.Label, p key.Position, timestamp time.Duratio
 		switch l {
 		case key.LabelKPEnter:
 			microtext.ToggleOpacity()
-		case key.LabelF11:
+		case key.LabelF11, key.LabelReturn:
 			internal.ToggleFullscreen()
 		}
 	}
