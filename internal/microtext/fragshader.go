e9f9059f5f029e15eb4fe795f5929fc739e5e257
Laurent Moussault <moussault.laurent@gmail.com>
2017-03-06 22:39:39 +0100

    Cleanup microtext shader

--- internal/microtext/fragshader.go
+++ internal/microtext/fragshader.go
@@ -28,43 +28,49 @@ layout(origin_upper_left) in vec4 gl_FragCoord;
 
 out vec4 Color;
 
-uint screenChar(int col, int row) {
-  int b = col + row * screen.NbCols; // The byte we're looking for
-  uint v = screen.Chars[b >> 2];
-  v = v >> (8 * (b & 0x3));
-  return v & 0xFF;
-}
-
 void main(void) {
+  int x = int(gl_FragCoord.x - screen.Left) / screen.PixelSize;
+  if (gl_FragCoord.x < screen.Left) {discard;}
+  int y = int(gl_FragCoord.y - screen.Top)  / screen.PixelSize;
+	if (gl_FragCoord.y < screen.Top) {discard;}
+  int col = x / charWidth;
+	if (col >= screen.NbCols) {discard;}
+  int row = y / charHeight;
+	if (row >= screen.NbRows) {discard;}
+
+	// 1: Lookup the char in text SSBO
+
+	// First, find the index of desired byte
+  int chrI = col + row * screen.NbCols;
+	// Fetch the word
+  uint chrB = screen.Chars[chrI >> 2];
+	// Isolate the correct byte inside the word
+  chrB = chrB >> (8 * (chrI & 0x3));
+  uint chr = chrB & 0xFF;
+
+	if (chr == 0) {discard;}
+
 	vec4 fg = vec4(screen.Foreground, 1.0);
 	vec4 bg = screen.Background;
-
-  int x = int(gl_FragCoord.x - screen.Left) / screen.PixelSize;
-  int y = int(gl_FragCoord.y - screen.Top)  / screen.PixelSize;
-  int col = x / charWidth;
-  int row = y / charHeight;
-
-  if (gl_FragCoord.x < screen.Left || gl_FragCoord.y < screen.Top ||
-		col >= screen.NbCols || row >= screen.NbRows) {
-    discard;
-  }
-
-  uint chr = screenChar(col, row);
 
   int dx = x - col*charWidth;
   int dy = y - row*charHeight;
 
-	// Calculate color
+	// 2: Lookup the char bitmap in font SSBO
 
-	// First, find the desired byte in font SSBO
-  int ib = int(chr) * charHeight + dy; // byte index
-  uint b = font.Data[ib >> 2];
-  b = b >> (8 * (ib & 0x3));
-	b &= 0xFF;
+	// First, find the index of desired byte
+  int fntI = int(chr) * charHeight + dy;
+	// Fetch the word
+  uint fntB = font.Data[fntI >> 2];
+	// Isolate the correct byte inside the word
+  fntB = fntB >> (8 * (fntI & 0x3));
+	fntB &= 0xFF;
+	// Isolate the correct bit inside the byte
+	uint fnt = (fntB >> (7 - dx)) & 0x1;
+
 	// Calculate the color
-	uint v = (b >> (7 - dx)) & 0x1;
-	bg.a *= float(b & 0x01);
-	Color = v * fg + (1 - v) * bg;
+	bg.a *= float(fntB & 0x01);
+	Color = fnt * fg + (1 - fnt) * bg;
 
 	if (Color.a == 0) {
 		discard;
