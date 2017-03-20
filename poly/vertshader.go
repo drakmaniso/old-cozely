7e7615c2c368bffaef0130f26c9008f2776d9a1d
Laurent Moussault <moussault.laurent@gmail.com>
2017-03-20 10:03:38 +0100

    Cleanup vertex shader

--- poly/vertshader.go
+++ poly/vertshader.go
@@ -11,26 +11,27 @@ package poly
 const vertshader = `
 #version 450 core
 
-layout(std140, binding = 0) uniform Frame {
+layout(std140, binding = 0) uniform frameBlock {
 	mat4 Projection;
 	mat4 View;
 	mat4 Model;
 } frame;
 
-struct facetight {
-	uint mathivert0vert1;
-	uint matlovert2vert3;
+struct face {
+	uint MatHiVert0Vert1;
+	uint MatLoVert2Vert3;
 };
-layout(std430, binding = 0) buffer FaceSSBO {
-	facetight []faces;
-} faceSSBO;
+layout(std430, binding = 0) buffer FaceBuffer {
+	face []Faces;
+} faceBuffer;
 
 struct vec3tight {
+	// Needed because vec3 arrays are not tighlty packed on some hardware
 	float x, y, z;
 };
-layout(std430, binding = 1) buffer VertexSSBO {
-	vec3tight []vertices;
-} vertexSSBO;
+layout(std430, binding = 1) buffer VertexBuffer {
+	vec3tight []Vertices;
+} vertexBuffer;
 
 out gl_PerVertex {
 	vec4 gl_Position;
@@ -41,37 +42,46 @@ out PerVertex {
 	layout(location = 1) flat out uint Material;
 } vertex;
 
-const uint [6]triangulate = {0, 1, 2, 0, 2, 3};
-
 void main(void) {
+	// Calculate index in face buffer
 	uint faceID = gl_VertexID / 6;
+	// Determine which face vertex this is
+	const uint [6]triangulate = {0, 1, 2, 0, 2, 3};
 	uint currVert = triangulate[gl_VertexID - (6 * faceID)];
 
-	facetight face = faceSSBO.faces[faceID];
-	uint vertID[4];
-	vertID[1] = face.mathivert0vert1 & 0x3FFF;
-	face.mathivert0vert1 >>= 14;
-	vertID[0] = face.mathivert0vert1 & 0x3FFF;
-	face.mathivert0vert1 >>= 14;
-	vertID[3] = face.matlovert2vert3 & 0x3FFF;
-	face.matlovert2vert3 >>= 14;
-	vertID[2] = face.matlovert2vert3 & 0x3FFF;
-	face.matlovert2vert3 >>= 14;
-	vertex.Material = face.mathivert0vert1 << 4 | face.matlovert2vert3;
+	// Read the face buffer
+	face f = faceBuffer.Faces[faceID];
 
-	vec3tight verts[4];
-	verts[0] = vertexSSBO.vertices[vertID[0]];
-	verts[1] = vertexSSBO.vertices[vertID[1]];
-	verts[2] = vertexSSBO.vertices[vertID[2]];
-	verts[3] = vertexSSBO.vertices[vertID[3]];
+	// Compute indices for the vertex buffer
+	uint vi[4];
+	vi[1] = f.MatHiVert0Vert1 & 0x3FFF;
+	f.MatHiVert0Vert1 >>= 14;
+	vi[0] = f.MatHiVert0Vert1 & 0x3FFF;
+	f.MatHiVert0Vert1 >>= 14;
+	vi[3] = f.MatLoVert2Vert3 & 0x3FFF;
+	f.MatLoVert2Vert3 >>= 14;
+	vi[2] = f.MatLoVert2Vert3 & 0x3FFF;
+	f.MatLoVert2Vert3 >>= 14;
+	vertex.Material = f.MatHiVert0Vert1 << 4 | f.MatLoVert2Vert3;
+
+	// Read the vertex buffer
+	vec3tight v[4];
+	v[0] = vertexBuffer.Vertices[vi[0]];
+	v[1] = vertexBuffer.Vertices[vi[1]];
+	v[2] = vertexBuffer.Vertices[vi[2]];
+	v[3] = vertexBuffer.Vertices[vi[3]];
+
+	// Compute world coordinates for all 4 vertices
 	vec3 p[4];
-	p[0] = vec3(frame.Model * vec4(verts[0].x, verts[0].y, verts[0].z, 1.0));
-	p[1] = vec3(frame.Model * vec4(verts[1].x, verts[1].y, verts[1].z, 1.0));
-	p[2] = vec3(frame.Model * vec4(verts[2].x, verts[2].y, verts[2].z, 1.0));
-	p[3] = vec3(frame.Model * vec4(verts[3].x, verts[3].y, verts[3].z, 1.0));
+	p[0] = vec3(frame.Model * vec4(v[0].x, v[0].y, v[0].z, 1.0));
+	p[1] = vec3(frame.Model * vec4(v[1].x, v[1].y, v[1].z, 1.0));
+	p[2] = vec3(frame.Model * vec4(v[2].x, v[2].y, v[2].z, 1.0));
+	p[3] = vec3(frame.Model * vec4(v[3].x, v[3].y, v[3].z, 1.0));
 
+	// Compute face normal (in world coordinates)
 	vertex.Normal = normalize(cross(p[2] - p[0], p[3] - p[1]));
 
+	// Compute screen coordinates
 	gl_Position = frame.Projection * frame.View * vec4(p[currVert], 1);
 }
 `
