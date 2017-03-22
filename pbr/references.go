a136185a82ab06d5e36761240741329740fec542
Laurent Moussault <moussault.laurent@gmail.com>
2017-03-22 08:46:13 +0100

    Add a few pbr references

---
+++ pbr/references.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2013-2017 Laurent Moussault. All rights reserved.
+// Licensed under a simplified BSD license (see LICENSE file).
+
+package pbr
+
+//------------------------------------------------------------------------------
+
+import "github.com/drakmaniso/glam/color"
+
+//------------------------------------------------------------------------------
+
+var (
+	Gold      = color.RGB{1.000000, 0.765557, 0.336057}
+	Silver    = color.RGB{0.971519, 0.959915, 0.915324}
+	Aluminium = color.RGB{0.913183, 0.921494, 0.924524}
+	Copper    = color.RGB{0.955008, 0.637427, 0.538163}
+	Chromium  = color.RGB{0.549585, 0.556114, 0.554256}
+	Nickel    = color.RGB{0.659777, 0.608679, 0.525649}
+	Titanium  = color.RGB{0.541931, 0.496791, 0.449419}
+	Cobalt    = color.RGB{0.662124, 0.654864, 0.633732}
+	Platinum  = color.RGB{0.672411, 0.637331, 0.585456}
+	Iron      = color.RGB{0.56, 0.57, 0.58} // To verify
+)
+
+//------------------------------------------------------------------------------
