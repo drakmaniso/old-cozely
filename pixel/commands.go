1c81282239d2431203459ad5ab3d651c8d603541
Laurent Moussault <moussault.laurent@gmail.com>
2018-01-04 18:43:32 +0100

    Add design doc for future pixel shader

---
+++ pixel/commands.go
@@ -0,0 +1,110 @@
+// Copyright (c) 2013-2017 Laurent Moussault. All rights reserved.
+// Licensed under a simplified BSD license (see LICENSE file).
+
+package pixel
+
+//------------------------------------------------------------------------------
+
+/*
+type paramIndexedPicture struct {
+	binflip int16
+	tx      int16
+	ty      int16
+	tw      int16
+	th      int16
+	x       int16
+	y       int16
+}
+
+type paramFullIndexedPicture struct {
+	binflip          int16
+	tx               int16
+	ty               int16
+	tw               int16
+	th               int16
+	x                int16
+	y                int16
+	w                int16
+	h                int16
+	transform, shift uint8
+}
+
+type paramRGBAPicture struct {
+	binfip int16
+	tx     int16
+	ty     int16
+	tw     int16
+	th     int16
+	x      int16
+	y      int16
+}
+
+type paramFulRGBAPicturel struct {
+	binflip          int16
+	tx               int16
+	ty               int16
+	tw               int16
+	th               int16
+	x                int16
+	y                int16
+	w                int16
+	h                int16
+	transform, alpha uint8
+}
+
+//------------------------------------------------------------------------------
+
+type paramIndexedPoint struct {
+	color, _ uint8
+	x        int16
+	y        int16
+}
+
+type paramPoint struct {
+	rg uint16
+	ba uint16
+	x  int16
+	y  int16
+}
+
+type paramIndexedLine struct {
+	color, width uint8
+	x1           int16
+	y1           int16
+	x2           int16
+	y2           int16
+}
+
+type paramRGBALine struct {
+	rg               uint16
+	ba               uint16
+	x1               int16
+	y1               int16
+	x2               int16
+	y2               int16
+	width, antialias uint8
+}
+
+type paramRGBABezier struct {
+	rg               uint16
+	ba               uint16
+	x1               int16
+	y1               int16
+	x2               int16
+	y2               int16
+	x3               int16
+	y3               int16
+	x4               int16
+	y4               int16
+	width, antialias uint8
+}
+
+*/
+
+//------------------------------------------------------------------------------
+
+const (
+	cmdIndexedPoint uint32 = 1 << 2
+)
+
+//------------------------------------------------------------------------------
