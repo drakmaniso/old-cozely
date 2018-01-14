63b464e19b661094f0385cbb621326e9417b6f02
Laurent Moussault <moussault.laurent@gmail.com>
2018-01-14 18:50:28 +0100

    Update glam package doc

--- doc.go
+++ doc.go
@@ -2,6 +2,13 @@
 // Licensed under a simplified BSD license (see LICENSE file).
 
 /*
-Package glam provides a framework for making pixel-art games in Go.
+Package glam will provide a minimalist framework for making games in Go, using
+2D pixel art or 3D polygonal art (aka low-poly).
+
+Work in Progress
+
+**Not yet in a usable state**: the framework is *very* incomplete, and the API
+is subject to frequent changes.
+
 */
 package glam
