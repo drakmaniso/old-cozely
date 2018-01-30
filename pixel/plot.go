ab70aee63056c2b24e4e705e80840dd8db8005df
Laurent Moussault <moussault.laurent@gmail.com>
2018-01-30 14:57:09 +0100

    Remove pixel.Text

--- pixel/plot.go
+++ pixel/plot.go
@@ -4,8 +4,6 @@
 package pixel
 
 import (
-	"unicode/utf8"
-
 	"github.com/drakmaniso/glam/palette"
 )
 
@@ -13,40 +11,6 @@ import (
 
 func (s *ScreenCanvas) Picture(p Picture, x, y int16) {
 	s.appendCommand(cmdPicture, 4, 1, int16(p), s.origin.X+x, s.origin.Y+y)
-}
-
-//------------------------------------------------------------------------------
-
-func (s *ScreenCanvas) Text(f Font, c palette.Index, x, y int16, txt string) int16 {
-	done := int(0)
-	xx, yy := s.origin.X+x, s.origin.Y+y
-	for done < utf8.RuneCountInString(txt) {
-		t := []int16{int16(f), int16(c), xx, yy} //TODO: remove allocation
-		dx := uint16(0)
-		i := 0
-		for _, r := range txt {
-			i++
-			if i <= done {
-				continue
-			}
-			rr := uint16(0x7F)
-			if r <= 0x7F {
-				rr = uint16(r)
-			}
-			_, _, _, rw, _ := f.getMap(rune(rr))
-			rr |= dx << 7
-			t = append(t, int16(rr))
-			dx += uint16(rw) + 0
-			done++
-			if dx > 0x1FF {
-				break
-			}
-		}
-		s.appendCommand(cmdPrint, 4, uint32(len(t)-4), t...)
-		xx += int16(dx)
-	}
-
-	return xx + 7 - x
 }
 
 //------------------------------------------------------------------------------
