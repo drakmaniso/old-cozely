27f4db938f53901c3cafaab9af188413b26f7b6a
drakmaniso <moussault.laurent@gmail.com>
2018-04-06 14:47:47 +0200

    Add a few tests for quadedge.Delaunay

--- plane/quadedge/delaunay_test.go
+++ plane/quadedge/delaunay_test.go
@@ -1,9 +1,12 @@
 package quadedge_test
 
 import (
+	"github.com/drakmaniso/glam/x/math32"
 	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/drakmaniso/glam/key"
 
 	"github.com/drakmaniso/glam"
 	"github.com/drakmaniso/glam/colour"
@@ -165,6 +168,50 @@ func newPoints() {
 		points[i] = plane.Coord{X: rand.Float32(), Y: rand.Float32()}
 	}
 	triangulation = quadedge.Delaunay(points)
+}
+
+//------------------------------------------------------------------------------
+
+func (dl delLoop) KeyDown(l key.Label, p key.Position) {
+	switch l {
+	case key.Label1:
+		points = points[:0]
+		const st = 1.0 / 16
+		for x := float32(st); x < 1.0; x += st {
+			for y := float32(st); y < 1.0; y += st {
+				points = append(points, plane.Coord{X: x, Y: y})
+			}
+		}
+		triangulation = quadedge.Delaunay(points)
+	case key.Label2:
+		points = points[:0]
+		for a := float32(0); a < 2*math32.Pi; a += math32.Pi/8 {
+				points = append(points, plane.Coord{
+					X: .5-math32.Cos(a)*.5,
+					Y: .5+math32.Sin(a)*.5,
+				})
+		}
+		triangulation = quadedge.Delaunay(points)
+	case key.Label3:
+		points = points[:0]
+		const n = 26
+		for a := float32(0); a < n*2*math32.Pi; a += math32.Pi/26 {
+				points = append(points, plane.Coord{
+					X: .5+math32.Cos(a)*.5*a/(n*2*math32.Pi),
+					Y: .5+math32.Sin(a)*.5*a/(n*2*math32.Pi),
+				})
+		}
+		triangulation = quadedge.Delaunay(points)
+	case key.Label4:
+		points = points[:0]
+		const st = 1.0 / 6
+		for x := float32(st); x < 1.0; x += st {
+				points = append(points, plane.Coord{X: x, Y: 0.5 + 0.17*x})
+		}
+		triangulation = quadedge.Delaunay(points)
+	default:
+		dl.Handlers.KeyDown(l, p)
+	}
 }
 
 //------------------------------------------------------------------------------
