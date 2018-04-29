b9ce20daf51ce3830900eb2a3ad38a2a0c0889c0
drakmaniso <moussault.laurent@gmail.com>
2018-04-29 15:40:36 +0200

    Tweak example

--- ze2_interactive_test.go
+++ ze2_interactive_test.go
@@ -12,29 +12,27 @@ import (
 // Declarations ////////////////////////////////////////////////////////////////
 
 var (
-	quit  = input.Digital("Quit")
-	start = input.Digital("Start")
+	quit = input.Digital("Quit")
+	play = input.Digital("Play")
 )
 
 var bindings = input.Bindings{
 	"Default": {
-		"Start": {"Space", "Mouse Left", "Button A"},
-		"Quit":  {"Escape", "Button Back"},
+		"Play": {"Space", "Mouse Left", "Button A"},
+		"Quit": {"Escape", "Button Back"},
 	},
 }
 
 var (
-	canv       = pixel.Canvas(pixel.Resolution(160, 100))
+	canvas2    = pixel.Canvas(pixel.Resolution(160, 100))
 	logo       = pixel.Picture("graphics/cozely")
 	monochrome = color.PaletteFrom("graphics/cozely")
 	colorful   = color.Palette()
 )
 
-var started = false
-
-var reverse = false
-
-type loop2 struct{}
+type loop2 struct {
+	playing bool
+}
 
 // Initialization //////////////////////////////////////////////////////////////
 
@@ -43,16 +41,16 @@ func Example_interactive() {
 
 	input.Load(bindings)
 	cozely.Configure(cozely.UpdateStep(1.0 / 3))
-	err := cozely.Run(loop2{})
+	err := cozely.Run(&loop2{})
 	if err != nil {
 		panic(err)
 	}
 	// Output:
 }
 
-func (loop2) Enter() {
+func (l *loop2) Enter() {
 	monochrome.Activate()
-	shufflecolors()
+	l.shufflecolors()
 }
 
 func (loop2) Leave() {
@@ -60,50 +58,41 @@ func (loop2) Leave() {
 
 // Game Loop ///////////////////////////////////////////////////////////////////
 
-func (loop2) React() {
-	if start.Started(input.Any) {
-		started = !started
+func (l *loop2) React() {
+	if play.Started(input.Any) {
+		l.playing = !l.playing
 	}
 	if quit.Started(input.Any) {
 		cozely.Stop(nil)
 	}
 }
 
-func (loop2) Update() {
-	if started {
-		shufflecolors()
+func (l *loop2) Update() {
+	if l.playing {
+		l.shufflecolors()
 	}
 }
 
-func shufflecolors() {
-	dark := [12]bool{
-		true, false, true, false, true, false,
-		false, true, false, true, false, true,
-	}
-	reverse = !reverse
+func (l *loop2) shufflecolors() {
 	for i := 2; i < 14; i++ {
-		g := 0.2 + 0.8*rand.Float32()
-		r := 0.2 + 0.8*rand.Float32()
-		b := 0.2 + 0.8*rand.Float32()
-		if dark[i-2] != reverse {
-			colorful.Set(uint8(i), color.SRGB{r, g, b})
-		} else {
-			colorful.Set(uint8(i), color.LRGB{r, g, b})
-		}
+		g := 0.2 + 0.7*rand.Float32()
+		r := 0.2 + 0.7*rand.Float32()
+		b := 0.2 + 0.7*rand.Float32()
+		colorful.Set(uint8(i), color.SRGB{r, g, b})
 	}
 }
 
-func (loop2) Render() {
-	canv.Clear(0)
+func (l *loop2) Render() {
+	canvas2.Clear(0)
 
-	if started {
+	if l.playing {
 		colorful.Activate()
 	} else {
 		monochrome.Activate()
 	}
 
-	o := canv.Size().Minus(logo.Size()).Slash(2)
-	canv.Picture(logo, 0, o)
+	o := canvas2.Size().Minus(logo.Size()).Slash(2)
+	canvas2.Picture(logo, 0, o)
 
-	canv.Display()
+	canvas2.Display()
 }
