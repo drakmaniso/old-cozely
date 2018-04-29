e052536a2a2e50407b26b8a911f36ec3d73ca103
drakmaniso <moussault.laurent@gmail.com>
2018-04-29 16:30:48 +0200

    Convert second pixel example to stress test

--- pixel/z02_test.go
+++ pixel/z02_test.go
@@ -18,6 +18,7 @@ import (
 
 var (
 	palette2 = color.PaletteFrom("graphics/shape1")
+	txtColor = palette2.Entry(color.LRGB{1, 1, 1})
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -39,7 +40,7 @@ type shape struct {
 
 type loop2 struct{}
 
-var shapes [2048]shape
+var shapes = make([]shape, 350000)
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -61,6 +62,7 @@ func TestTest2(t *testing.T) {
 
 func (loop2) Enter() {
 	palette2.Activate()
+	canvas2.Text(txtColor, pixel.Monozela10)
 }
 
 func (loop2) Leave() {
@@ -70,7 +72,7 @@ func resize() {
 	s := canvas2.Size()
 	for i := range shapes {
 		j := rand.Intn(len(shapePictures))
-		shapes[i].depth = int16(j)
+		shapes[i].depth = int16(i)
 		p := shapePictures[j]
 		shapes[i].pict = p
 		shapes[i].pos.C = int16(rand.Intn(int(s.C - p.Size().C)))
@@ -81,6 +83,58 @@ func resize() {
 ////////////////////////////////////////////////////////////////////////////////
 
 func (loop2) React() {
+	if scene1.Started(0) {
+		shapes = make([]shape, 1000)
+		resize()
+	}
+	if scene2.Started(0) {
+		shapes = make([]shape, 10000)
+		resize()
+	}
+	if scene3.Started(0) {
+		shapes = make([]shape, 100000)
+		resize()
+	}
+	if scene4.Started(0) {
+		shapes = make([]shape, 200000)
+		resize()
+	}
+	if scene5.Started(0) {
+		shapes = make([]shape, 300000)
+		resize()
+	}
+	if scene6.Started(0) {
+		shapes = make([]shape, 350000)
+		resize()
+	}
+	if scene7.Started(0) {
+		shapes = make([]shape, 400000)
+		resize()
+	}
+	if scene8.Started(0) {
+		shapes = make([]shape, 450000)
+		resize()
+	}
+	if scene9.Started(0) {
+		shapes = make([]shape, 500000)
+		resize()
+	}
+	if scene10.Started(0) {
+		shapes = make([]shape, 10)
+		resize()
+	}
+	if next.Started(0) {
+		shapes = append(shapes, shape{})
+		i := len(shapes) - 1
+		j := rand.Intn(len(shapePictures))
+		shapes[i].depth = int16(i)
+		p := shapePictures[j]
+		shapes[i].pict = p
+		shapes[i].pos = canvas2.FromWindow(cursor.XY(0).CR()).Minus(p.Size().Slash(2))
+	}
+	if previous.Started(0) {
+		shapes = shapes[:len(shapes)-1]
+	}
 	if quit.Started(0) {
 		cozely.Stop(nil)
 	}
@@ -92,11 +146,17 @@ func (loop2) Update() {
 
 func (loop2) Render() {
 	canvas2.Clear(0)
-	for i, o := range shapes {
-		if float64(i)/32 > cozely.GameTime() {
-			break
-		}
+	for _, o := range shapes {
+		// if float64(i)/32 > cozely.GameTime() {
+		// 	break
+		// }
 		canvas2.Picture(o.pict, o.depth, o.pos)
+	}
+	canvas2.Locate(0x7FFF, coord.CR{8, 16})
+	ft, ov := cozely.RenderStats()
+	canvas2.Printf("%dk pictures: %6.2f", len(shapes)/1000, ft*1000)
+	if ov > 0 {
+		canvas2.Printf(" (%d)", ov)
 	}
 	canvas2.Display()
 }
