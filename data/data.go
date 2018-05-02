a5cac036f73317364892679a789fb5b7949c721b
drakmaniso <moussault.laurent@gmail.com>
2018-05-02 22:51:02 +0200

    Improve czl format description

--- data/data.go
+++ data/data.go
@@ -1,48 +1,92 @@
 package data
 
 import (
+	"errors"
 	"io"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
 
 type Data interface {
+	Label() []byte
+	Source() (line, col int)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-type String []byte
+type String struct {
+	label     []byte
+	value     []byte
+	line, col uint32
+}
+
+func (a String) Label() []byte {
+	return a.label
+}
+
+func (a String) Source() (line, col int) {
+	return int(a.line), int(a.col)
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
 type List struct {
-	items []struct {
-		Data
-		line, col uint32
-	}
-	pairs map[string]struct {
-		Data
-		line, col uint32
+	label      []byte
+	items      []Data
+	dictionary map[string][]Data
+	line, col  uint32
+}
+
+func Parse(source io.Reader) List {
+	//TODO
+	return List{}
+}
+
+// Items returns a slice of all the items of the list (with or without
+// labels).
+func (a List) Items() []Data {
+	return a.items
+}
+
+// WithLabel returns a slice of all items with a specific label in the list.
+func (a List) WithLabel(label string) []Data {
+	return a.dictionary[label]
+}
+
+// WithName tries to find the only item in the list with a specific label. If
+// not present, nil is returned. If multiple items have the same label, the last
+// one is returned, as well as an error.
+func (a List) WithName(label string) (Data, error) {
+	d := a.dictionary[label]
+	switch len(d) {
+	case 0:
+		return nil, nil
+	case 1:
+		return d[0], nil
+	default:
+		return d[len(d)-1], errors.New("ambiguous data: label \"" + label + "\" is used multiple times inside the same list")
 	}
 }
 
-func Parse(source io.Reader) *List {
-	//TODO
+// WithoutLabel return the n-th unlabeled item of the list (or nil if there
+// isn't enough items).
+func (a List) WithoutLabel(n int) Data {
+	j := 0
+	for range a.items {
+		if j == n {
+			return a.items[j]
+		}
+		if len(a.items[j].Label()) > 0 {
+			j++
+		}
+	}
 	return nil
 }
 
-func (a *List) At(index int) Data {
-	return a.items[index].Data
+func (a List) Label() []byte {
+	return a.label
 }
 
-func (a *List) With(key string) Data {
-	return a.pairs[key].Data
-}
-
-func (a *List) LocationAt(index int) (line, col int) {
-	return int(a.items[index].line), int(a.items[index].col)
-}
-
-func (a *List) LocationWith(key string) (line, col int) {
-	return int(a.pairs[key].line), int(a.pairs[key].col)
+func (a List) Source() (line, col int) {
+	return int(a.line), int(a.col)
 }
--- data/doc.go
+++ data/doc.go
@@ -8,14 +8,14 @@ This is currently only a proof of concept, with no implementation.
 
 Comments
 
-Comments start with two pipe characters "||", and end with newline:
+Comments start with two semicolons ";;", and end with newline:
 
-  || This whole line is a comment
-  (1, 2, 3) || But comments can also occur at end of line
+  ;; This whole line is a comment
+  (1, 2, 3) ;; Comment on the same line as meaningful data
 
-An isolated pipe character is considered ambiguous:
+An isolated semicolon is considered ambiguous:
 
-  Red | Blue || Ambiguous! (interpreted as "Red | Blue"; see quoted strings)
+  this is ; Ambiguous! (but still interpreted as a comment)
 
 Strings
 
@@ -33,9 +33,9 @@ of data considered atomic.
 Unquoted strings cannot contain newlines or special characters. Blank spaces are
 allowed, but are stripped at the begininng and the end of the value.
 
-The 16 special characters:
+The 13 special characters:
 
-  | , : = " @ & $
+  ; , : = "
   ( ) [ ] { } < >
 
 Note that backslash is not a special character! The following is allowed:
@@ -49,13 +49,13 @@ allowed except newline and the double quote:
 
 To insert a double quote character, use two in succession:
 
-  "Here is a ""quote"" inside a string." || Here is a "quote" inside a string.
+  "Here is a ""quote"" inside a string." ; Here is a "quote" inside a string.
 
-To allow newlines, put a newline immediatelu after the opening double quote, and
+To allow newlines, put a newline immediately after the opening double quote, and
 another immediately before the closing one:
 
   "This is
-  wrong..." || Ambiguous! (newline in quoted value)
+  wrong..." ;; Ambiguous! (newline in quoted value)
 
   "
   This, however, is a
@@ -97,17 +97,17 @@ end-of-line are optional:
 It is however an error to use two comas in a row. To correctly insert an empty
 item, an empty quoted string "" should be used:
 
-  (first, , third) || Ambiguous! (interpreted as three items)
-  (first, "", third) || Second item is empty
+  (first, , third)   ;; Ambiguous! (interpreted as three items)
+  (first, "", third) ;; Second item is empty
 
 It is also an error to use a coma just before a bracket, except if there is a
 newline between them:
 
-  (first, second, ) || Ambiguous! (interpreted as *two* items)
+  (first, second, ) ;; Ambiguous! (interpreted as *two* items)
 
   (
     first,
-    second, || Correct (two items)
+    second, ;; Correct (two items)
   )
 
 On the other hand, multiple newlines between items are allowed:
@@ -116,59 +116,159 @@ On the other hand, multiple newlines between items are allowed:
     first
 
 
-    second || Correct (two items)
+    second ;; Correct (two items)
   )
 
-Four different kind of brackets can be used, and are all equivalent: round
-brackets "()", square brackets "[]", curly brackets "{}", and angle brackets
-"<>". The following lists are all the same:
+Three different kind of brackets can be used, and are all equivalent: round
+brackets "()", square brackets "[]", and curly brackets "{}". The following
+lists are all the same:
 
   (first, second, third)
   [first, second, third]
   {first, second, third}
-  <first, second, third>
 
-The different brackets are especially useful to write nested lists:
+These different brackets are especially useful to write nested lists:
 
   (first, [s, e, c, o, n, d], third)
 
   (
     first
     [s, e, c, o, n, d]
-    [<t, h>, i, r ,d]
+    [{t, h}, i, r ,d]
   )
 
-In addition to simple items, lists can also contain key-value pairs. A pair is
-distinguished from a normal item by the presence of a colon ":" or an equal sign
-"=" between the key and the value:
+It is also possible to write lists with angle brackets "<>", but they have
+special meaning, and are *not* equivalent to the same list with traditional
+brackets:
 
-  (first item, second item, an: option, another: option)
+  <first, second, third> ;; Not the same list as the three above!
 
-Key-value pairs are not ordered, but they must occur after the last item:
+In fact, most of the time those angle-bracketed list will simply be ignored. The
+only thing the user need to know is that they obey the same parsing rules as the
+other lists. For advanced use of the czl format, application can activate an
+option in the parser to interpret them as macro lists (see the corresponding
+section).
 
-  (first, second, lang=english, count=2)
-  (first, second, count=2, lang=english) // Same list as above
-  (lang=english, first, count=2, second) // Ambiguous! (interpreted as above)
 
-It is an error to omit the key or the value of a pair. To specify an empty key
-or an empty value, use a quoted empty string:
+Labels
 
-  (1, 2, lang=) // Ambiguous (interpreted as an empty value)
-  (1, 2, lang="")
-  (1, 2, =arabic) // Ambigous (interpreted as an empty key)
-  (1, 2, ""=arabic)
+Both strings and lists can, optionally, be prefixed with a label. The label and
+its value are separated by either a colon ":" or an equal sign "=":
 
-In a key-value pair, the value can be either a string or a list, but the key
-must be a string:
+  label: in front of a string
+  (and, this, is, also, a, labeled=string, inside, a, list)
 
-  (first, second, lang=english)
-  (first, second, lang=[british, american])
-  (first, second, [lang, language]=english) || Ambiguous! (the pair is ignored)
+  label: (in, front, of, a, list)
+  label (in, front, of, a, list)  ;; Ambiguous! (interpreted as above)
 
-In a given list, all key-value pairs must have unique keys:
+Unquoted labels cannot not contain any blank space, newline, or special
+characters. Use quotes if you need them:
 
-  (first, second, lang=british, lang=american) || Ambiguous! (second pair overrides the first)
-  (first, first, first) || Correct: items don't need to be unique
+  "this is": correct
+  this is: wrong     ;; Ambiguous! (interpreted as above)
+
+  "this: is (also)" = correct
+
+  "
+  and this is a
+  multi-line label!
+  " : unusual
+
+Both the label and its value are mandatory. To specify an empty value, use the
+empty string "":
+
+  lang=""
+  label=   ;; Ambiguous (interpreted as above: empty string value)
+
+The empty string can also be used as a label, but is equivalent to an unlabeled
+value:
+
+  value
+  ""=value ;; Same as above
+  =value   ;; Ambigous (interpreted as above: no label)
+
+In a given list, the same label can occur multiple times. The correctness of
+this is left to the application:
+
+  (first, second, lang=british, lang=american) ;; Correct, depending on the application
+  (first, _, third, count=2, count=3)          ;; Ambiguous, depending on the application
+  (first, first, first)                        ;; Correct: items don't need to be unique
+
+
+Macros
+
+Any list written with angle brackets is parsed as a macro or an expression. By
+default, they will simply be ignored; if they have a label, it is also ignored.
+An option in the parser has to be turned on in order to use their functionality.
+
+  <pi=3.14159>  ;; Defines a variable (evaluates to nothing)
+  <pi>          ;; Evaluates to "3.14157"
+
+  <vowels=(a, e, i, o, u, y)>  ;; Defines a list constant (evaluates to nothing)
+  <vowels>                     ;; Evaluates to the list (a, e, i, o, u, y)
+
+  <$foo>     ;; Defines a named anchor (evaluates to nothing)
+
+  /0         ;; The first top-level item
+  /1         ;; The second top-level item
+  /0/0       ;; The first item in the first top-level item (which must be a list)
+  .          ;; The current list
+  ./.        ;; The current item
+  ..         ;; The parent list
+  ../..      ;; The parent of the parent list
+  ...        ;; The top-level ancestor list
+  .../.      ;; The current item in the top-level ancestor list
+  ./0        ;; The first item in the current list
+  ./+1       ;; The next item in the current list
+  ./-1       ;; The previous item in the current list
+  ../0       ;; The first item in the parent list
+  ../+1      ;; The item following the current list
+  foo        ;; The item containing the anchor foo
+  foo/..     ;; The list containing the anchor foo
+
+  <#.>       ;; Index of the current list (position in the parent list)
+  <#..>      ;; Index of the parent list (position in the grand-parent list)
+  <#../+1>   ;; Index of the item following the current list
+  <#...>     ;; Index of the top-level parent list
+  <#.../.>   ;; Index of the current item of the top-level parent list
+  <#foo>     ;; Index of the string containing anchor foo
+  <#foo/..>  ;; Index of the list containing anchor foo
+
+  <@/0/0>    ;; Copy of the first item of the first top-level item
+  <@./0>     ;; Copy of the first item of the current list
+  <@./0/0>   ;; Copy of the first item of the first item of the current list
+  <@./+1>    ;; Copy of the following item
+  <@../+1>   ;; Copy of the item following the current list
+  <@.../+1>  ;; Copy of the next top-level item
+  <@foo>     ;; Copy of the item following anchor foo
+  <@foo/..>  ;; Copy of the list containing anchor foo
+
+
+Expressions
+
+  <1 2 +>         ;; "3"
+  <a=2 a>       ;; "2"
+  <1 1 a=+ a>   ;; "2"
+  <a=2 a ++>    ;; "11"
+  <1 2 3 +>       ;; Error! (the stack contains 2 items at the end of the expression)
+  <1 2 3 + !>     ;; Two items: "1", "5"
+  <pi 2 *>       ;; "6.28318"
+  <"foo" "bar" ~>     ;; "foobar"
+  <"foo" 33 ~>      ;; "foo33"
+  <111 33 ~>      ;; "11133"
+  <# 1 +>         ;; Index of the current item plus one
+  <1 # +>         ;; Same as above
+  <# 1 + 2 *>     ;; Double of the index of the current item
+
+  <pi 3.14159 ?eq>  ;; "true"
+  <3 44 ?gt>         ;; "false"
+
+  <true then:"foo">              ;; "foo"
+  <false then:"foo">             ;; Nothing
+  <false then:"foo" else:"bar">  ;; "bar"
+
+  <0 for:3 do:<10 +>>                   ;; "30"
+  <"" i=2 for:<i 5 ?lt> do:<i ~ i ++>>   ;; "234"
 
 */
 package data
