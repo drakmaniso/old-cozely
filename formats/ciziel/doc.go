a04f898959308979534b04f39f744880102b4ef4
drakmaniso <moussault.laurent@gmail.com>
2018-09-16 22:27:13 +0200

    Add package description

--- formats/ciziel/doc.go
+++ formats/ciziel/doc.go
@@ -1,5 +1,8 @@
 /*
-Package ciziel provides a parser for the ".czl" configuration format
+Package ciziel provides a parser for a relaxed version of the JSON format
+
+Ciziel is a superset of JSON. This means that it can parse all valid JSON files,
+but also accept files with a relaxed, more human-friendly, syntax.
 
 
 WORK IN PROGRESS
